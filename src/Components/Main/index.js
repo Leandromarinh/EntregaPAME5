f98eba012451f73c485d4a2ef02eef808cf768dc
Leandromarinh <leandrinm14@gmail.com>
2021-02-09 13:07:19 -0300

    contador done

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -4,10 +4,13 @@ import './styles.css';
 
 import Contador from "../Contador";
 
+import ProductList from "../ProductList";
+
 export default function Main(){
     return(
         <div className="contador-container">
             <Contador/>
+            <ProductList/>
         </div>
 
     )
